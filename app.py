c2a92e6539cbf6f6c10358639a235f2ed0a3c27e
Sanjay Malladi <sanjaymalladi16@gmail.com>
2023-12-28 22:25:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 import movie_script
+from jinja2 import Environment
+
 
 app = Flask(__name__)
-
+app.jinja_env.globals['enumerate'] = enumerate
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -14,7 +16,7 @@ def search():
         movie_details = movie_script.search_movie(search_query)
 
         if movie_details:
-            recommendations = movie_script.recommend_movies(movie_details['id'])
+            recommendations = movie_script.recommend_movies(movie_details['id'])  # Add this line for debugging
             return render_template('result.html', movie_details=movie_details, recommendations=recommendations)
         else:
             return render_template('no_result.html')
